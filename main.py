e430a83f2c9e9c5f7af80f6e44a8f5c307c8a61e
Chen <chdangg@gmail.com>
2022-08-30 23:00:17 +0200

    add more buttons

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import font as tkFont
-import time
 from datetime import datetime
-
 from os.path import exists
 import csv
 
@@ -11,14 +9,20 @@ import csv
 class DataControl():
     def __init__(self):
         self.projectNames = set()
+        self.archivedProjectNames = set()
         self.trackRecord = {}
         self.workingProject = ""
 
         if exists('OTT_projects.txt'):
             with open('OTT_projects.txt','r') as f:
                 lines = [i.strip() for i in f.readlines()]
-                self.projectNames = set(lines)
-            print("projects: ", projectNames)
+                for l in lines:
+                    if l[:2] == "--":
+                        self.archivedProjectNames.add(l[2:])
+                    else:
+                        self.projectNames.add(l)
+            print("projects: ", self.projectNames)
+            print("archived projects: ", self.archivedProjectNames)
 
 
         log_file = '{}-{}.log'.format(datetime.now().year, datetime.now().month)
@@ -31,7 +35,6 @@ class DataControl():
                     else:
                         self.trackRecord[row[0]] = [(row[1], row[2], int(row[3]))]
         print(self.trackRecord)
-
 
     def getTodayTotal(self, name):
         if name in self.trackRecord:
@@ -63,6 +66,10 @@ class DataControl():
         with open(log_file, 'a') as f:
             f.write(self.workingProject + ", "+ str(startTime) + ", " + str(endTime) + ", " + str((endTime-startTime).seconds)+"\n")
 
+    def archiveProject(self, name):
+        pass
+
+
 
 
 class MainWindow():
@@ -75,36 +82,50 @@ class MainWindow():
 
         # ******** data **********
         self.data = DataControl()
+        self.projectButtons = []
+        self.projectLabels = []
+
+        # ******** Frames **********
+        self.projectArea = Frame()
+        self.projectArea.pack(side = TOP)
+        self.controlArea = Frame()
+        self.controlArea.pack(side = BOTTOM)
+        # self.settingArea = Frame()
+        # self.settingArea.pack(side = TOP)
+        # self.settingArea.pack_forget()
+
+        # ******** buttons **********
         for name in self.data.projectNames:
             self.addNewProjectButton(name)
 
-        # ******** widgets **********
-        self.projectButtons = []
-        self.projectLabels = []
-        self.centralArea = Frame()
-        self.centralArea.pack(side = TOP)
-        self.bottomBar = Frame()
-        self.bottomBar.pack(side = BOTTOM)
-
-        self.newItemButton = tk.Button(self.bottomBar, text='New')
+        self.newItemButton = tk.Button(self.controlArea, text='New')
         self.newItemButton.grid(column=0, row = 0)
         self.newItemButton.bind("<ButtonRelease>", lambda event: self.release(self.addNewItem))
 
-        self.exitButton = tk.Button(self.bottomBar, text='Exit')
+        self.exitButton = tk.Button(self.controlArea, text='Exit')
         self.exitButton.grid(column=1, row = 0)
-        self.exitButton.bind("<ButtonRelease>", lambda event: self.release(lambda: exit()))
+        self.exitButton.bind("<ButtonRelease>", lambda event: self.release(exit))
+
+        self.settingButton = tk.Button(self.controlArea, text='Setting')
+        self.settingButton.grid(column=0, row = 1)
+        self.settingButton.bind("<ButtonRelease>", lambda event: self.release(self.openSetting))
+
+        self.returnSettingButton = tk.Button(self.controlArea, text='Return')
+        self.returnSettingButton.bind("<ButtonRelease>", lambda event: self.release(self.closeSetting))
+
+        self.archiveButton = tk.Button(self.controlArea, text='Archive')
+        self.archiveButton.grid(column=1, row = 1)
+        self.archiveButton.bind("<ButtonRelease>", lambda event: self.release(self.archiveProject))
 
         self.photo = PhotoImage(file='stop.png').subsample(8,8)
-        self.stopButton = tk.Button(self.centralArea, image = self.photo)
+        self.stopButton = tk.Button(self.controlArea, image = self.photo)
         self.stopButton.bind("<ButtonRelease>", lambda event: self.release(self.stopWorking))
-        self.countingLabel = tk.Label(self.centralArea, text="")
+        self.countingLabel = tk.Label(self.controlArea, text="")
 
-        # ******** add buttons **********
         # ******** variables **********
         self.dragging = False
         self.startTime = 0
         self.tracking = False
-
 
 
     # ******** window behavior **********
@@ -125,12 +146,38 @@ class MainWindow():
         elif f != 0:
             f()
 
+    def openSetting(self):
+        self.newItemButton.grid_remove()
+        self.exitButton.grid_remove()
+        for button in self.projectButtons:
+            button.grid_remove()
+        for label in self.projectLabels:
+            label[1].grid_remove()
+        self.projectArea.pack_forget()
+        self.returnSettingButton.grid(column=0, row = 1)
+        # self.controlArea.pack_forget()
+
+    def closeSetting(self):
+        self.projectArea.pack()
+        self.newItemButton.grid()
+        self.exitButton.grid()
+        for button in self.projectButtons:
+            button.grid()
+        for label in self.projectLabels:
+            label[1].grid()
+        self.returnSettingButton.grid_remove()
+
+    def archiveProject(self):
+        pass
+
+
+
     # ******** add new project **********
     def addNewItem(self):
-        l = tk.Label(self.centralArea, text="Item Name ")
-        entry = tk.Entry(self.centralArea)
+        l = tk.Label(self.controlArea, text="Item Name ")
+        entry = tk.Entry(self.controlArea)
         entry.bind("<Return>", lambda event: self.confirmNewItem(entry, [l, entry, b]))
-        b = tk.Button(self.centralArea, text='Confirm New Item', command=lambda: self.confirmNewItem(entry, [l, entry, b]))
+        b = tk.Button(self.controlArea, text='Confirm New Item', command=lambda: self.confirmNewItem(entry, [l, entry, b]))
         l.grid(row = len(self.projectButtons)*2)
         entry.grid(row = len(self.projectButtons)*2 + 1)
         b.grid(row = len(self.projectButtons)*2 + 2)
@@ -146,27 +193,30 @@ class MainWindow():
 
     def addNewProjectButton(self, name):
         total = self.data.getTodayTotal(name)
-        new_button = tk.Button(self.centralArea, text=name, command= lambda: self.startWorking(new_button))
+        new_button = tk.Button(self.projectArea, text=name, command= lambda: self.startWorking(new_button))
         new_button.grid(row = len(self.projectButtons)*2)
-        l = tk.Label(self.centralArea, text="Today Total: {:02}:{:02}:{:02}".format(total//3600, total%3600//60, total%60))
+        l = tk.Label(self.projectArea, text="Today Total: {:02}:{:02}:{:02}".format(total//3600, total%3600//60, total%60))
         l.grid(row = len(self.projectButtons)*2 + 1)
         self.projectButtons.append(new_button)
         self.projectLabels.append((name, l))
-
-
 
     # ******** begin and stop **********
     def startWorking(self, button):
         self.data.startWorking(button['text'])
         self.startTime= datetime.now()
         self.tracking = True
+
+        self.projectArea.pack_forget()
         self.newItemButton.grid_remove()
         self.exitButton.grid_remove()
-        self.bottomBar.pack_forget()
+        self.settingButton.grid_remove()
+        self.archiveButton.grid_remove()
+        # self.controlArea.pack_forget()
         for button in self.projectButtons:
             button.grid_remove()
         for label in self.projectLabels:
             label[1].grid_remove()
+
         self.stopButton.grid(row = 0)
         self.countingLabel.grid(row = 1)
         self.countingLabel.after(1000, self.updateWorkingTime)
@@ -178,6 +228,7 @@ class MainWindow():
 
         self.data.stopWorking(self.startTime, endTime)
 
+        self.projectArea.pack()
         self.stopButton.grid_remove()
         self.countingLabel.grid_remove()
         for index in range(len(self.projectButtons)):
@@ -188,9 +239,11 @@ class MainWindow():
                 total = self.data.getTodayTotal(p[0])
                 p[1].configure(text = "Today Total: {:02}:{:02}:{:02}".format(total//3600, total%3600//60, total%60))
 
-        self.bottomBar.pack()
+        # self.controlArea.pack()
         self.newItemButton.grid()
         self.exitButton.grid()
+        self.settingButton.grid()
+        self.archiveButton.grid()
         self.m.attributes("-alpha", 1)
 
     # ******** update time label **********
